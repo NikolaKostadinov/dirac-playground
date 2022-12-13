97d7ca7a811a478db54f2f70b39a2e43d7c41176
Nikola Kostadinov <nikolakostadinov@protonmail.com>
2022-12-13 20:34:36 +0200

    this works for now

---
+++ include/params.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define WINDOW_TITLE    "Dirac Playground"
+#define WINDOW_WIDTH    720u
+#define WINDOW_HEIGHT   720u
+#define SIZE_X          72u
+#define SIZE_Y          72u
+#define START_X        -30.0f
+#define END_X           30.0f
+#define START_Y        -30.0f
+#define END_Y           30.0f
+#define INIT_X          0.0f
+#define INIT_Y          0.0f
+#define DEV             6.0f
+#define MOMENTUM_X      0.0f
+#define MOMENTUM_Y      0.0f
+#define DT              0.01f
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #define SDL_MAIN_HANDLED                                                // windows stuff
 
+#include "../include/params.h"
 #include "../include/Dirac/dirac.h"
 #include "../include/Griddy/griddy.h"
 #include "../include/SDL2/SDL.h"
@@ -9,27 +10,13 @@
 
 int main(int argc, char* args[])
 {
-    const char*    WINDOW_TITLE  = "Dirac Playground";
-    const uint32_t WINDOW_WIDTH  =  1000u            ;
-    const uint32_t WINDOW_HEIGHT =  1000u            ;
-    const uint32_t SIZE_X        =  60u              ;
-    const uint32_t SIZE_Y        =  60u              ;
-    const float    START_X       = -30.0f            ;
-    const float    END_X         =  30.0f            ;
-    const float    START_Y       = -30.0f            ;
-    const float    END_Y         =  30.0f            ;
-    const float    INIT_X        =  0.0f             ;
-    const float    INIT_Y        =  0.0f             ;
-    const float    DIV           =  6.0f             ;
-    const float    MOMENTUM_X    =  10000000000000.0f;
-    const float    MOMENTUM_Y    =  0.0f             ;
-    const float    DT            =  0.02f            ;
-
     Base       xBase = Base(START_X, SIZE_X, END_X);
     Base       yBase = Base(START_Y, SIZE_Y, END_Y);
     Basis2*    basis = new Basis2(xBase, yBase)    ;
+    //Scalar2*   u     = new Scalar2(basis)          ;
     WaveFunc2* psi   = new WaveFunc2(basis)        ;
 
+    //float          potnVals[SIZE_X][SIZE_Y];
     Complex        probAmps[SIZE_X][SIZE_Y];
     griddy::Vertex vertices[SIZE_X][SIZE_Y];
     
@@ -42,27 +29,28 @@ int main(int argc, char* args[])
     griddy::Grid grid = griddy::Grid(&window);
 
     SDL_Rect rectGrid = SDL_Rect();
-    rectGrid.x = 0            ;
-    rectGrid.y = 0            ;
-    rectGrid.w = WINDOW_WIDTH ;
-    rectGrid.h = WINDOW_HEIGHT;
+    rectGrid.x = 0                ;
+    rectGrid.y = 0                ;
+    rectGrid.w = WINDOW_WIDTH     ;
+    rectGrid.h = WINDOW_HEIGHT    ;
 
     grid.setVertices(&vertices[0][0]);
     grid.setPosition(rectGrid);
     grid.setSize(SIZE_X, SIZE_Y);
 
+    //u  ->setValues(&potnVals[0][0]);
     psi->setValues(&probAmps[0][0]);
+
     psi->setNorm(254.0f);
     psi->setMass(1.0f);
 
-    
     for (int i = 0; i < SIZE_X; i++)
         for (int j = 0; j < SIZE_Y; j++)
         {
             float x = xBase.x(i) - INIT_X;
             float y = yBase.x(j) - INIT_Y;
 
-            probAmps[i][j] = Complex(exp((-x*x-y*y) / DIV)) * cis(MOMENTUM_X * x + MOMENTUM_Y * y);
+            probAmps[i][j] = Real(exp((-x*x-y*y) / DEV)) * cis(MOMENTUM_X * x + MOMENTUM_Y * y);
             vertices[i][j] = griddy::Vertex(i, j);
         }
 
